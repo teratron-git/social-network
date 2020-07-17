b4a0eb3329c24c1445e019ce9ca9f981b2ec3449
Tereshkov Sergey[W] <rat87@mail.ru>
2020-07-17 16:36:40 +0300

    style: index.tsx codestyle

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './store/reducers';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './store/reducers'
+import './index.css'
+import App from './App'
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-            <App />
+      <App />
     </Provider>
-
   </React.StrictMode>,
-  document.getElementById('root') as HTMLElement
-);
+  document.getElementById('root')
+)
